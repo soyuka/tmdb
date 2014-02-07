c73a1fb52ceaa2460fd2b777343a0bc8b562ff96
soyuka@gmail.com <bluant@FGRINF003.ds-restauration.local>
2014-02-07 12:27:36 +0100

    first commit

---
+++ test/search.js
@@ -0,0 +1,45 @@
+var async = require('async');
+
+var tmdb = require('../lib/tmdb')('7c5105894b0446bb59b01a30cf235f3b', function(err, tmdb) {
+
+	async.series([
+		function(done) {
+			console.log('Search for Movies Harry Potter');
+			tmdb.search('movie', {query: 'Harry Potter', language: 'fr'}, function (err, results) {
+				if(err)
+					console.log(err);
+
+				console.log('Infos on first match');
+				tmdb.infos('movie', results.results[0].id, {language: 'fr'}, function (err, results) {
+					if(err)
+						console.log(err);
+
+					console.log(results);
+					done();
+
+				});
+			});
+		},
+		function(done) {
+			console.log('Search for TVSeries Leverage');
+			tmdb.search('tv', {query: 'Leverage', language: 'fr'}, function (err, results) {
+				if(err)
+					console.log(err);
+
+				console.log('Infos on first match');
+				tmdb.infos('tv', results.results[0].id, {language: 'fr'}, function (err, results) {
+					if(err)
+						console.log(err);
+
+					console.log(results);
+					done();
+				});
+			});
+		}
+	], function(err, results) {
+		console.log('Done testing with error', err);
+		process.exit(0);
+	});
+	
+});
+
